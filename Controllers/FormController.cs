877910f25a30229580e5c68de527c258486afab8
Kiah Daulton <59930638+kiahdaulton@users.noreply.github.com>
2023-03-31 15:03:45 -0500

    Safety Commit
    Safety commit before changing the datatype of the major name field to increase its size

---
+++ Controllers/FormController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HallHaven.Data;
+using HallHaven.Models;
+using Microsoft.AspNetCore.Identity;
+using HallHaven.Areas.Identity.Data;
+
+namespace HallHaven.Controllers
+{
+    public class FormController : Controller
+    {
+        private readonly HallHavenContext _context;
+        private readonly UserManager<HallHavenUser> _userManager;
+
+        public FormController(HallHavenContext context, UserManager<HallHavenUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Forms
+        public async Task<IActionResult> Index()
+        {
+            // get logged in user's gender through gender model
+            //.Where(g => g.Gender == genderId).
+
+            var hallHavenContext = _context.Forms.Include(f => f.CreditHour).Include(f => f.Dorm).Include(f => f.Major).Include(f => f.User);
+            return View(await hallHavenContext.ToListAsync());
+        }
+
+        // GET: Forms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Forms == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _context.Forms
+                .Include(f => f.CreditHour)
+                .Include(f => f.Dorm)
+                .Include(f => f.Major)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.FormId == id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+
+            return View(form);
+        }
+
+        // GET: Forms/Create
+        public IActionResult Create()
+        {
+            //ViewData["CreditHourId"] = new SelectList(_context.CreditHours, "CreditHourId", "CreditHourId");
+            ViewData["CreditHourId"] = new SelectList(_context.CreditHours, "CreditHourId", "CreditHourName");
+            // add in gender portion here
+            ViewData["DormId"] = new SelectList(_context.Dorms, "DormId", "DormName");
+            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorName");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Forms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FormId,DormId,UserId,MajorId,CreditHourId,GenderId,IsCandiateStudent,IsStudentAthlete,NeatnessLevel,VisitorLevel,FitnessLevel,AcademicLevel,SocialLevel,SharingLevel,BackgroundNoiseLevel,BedTimeRanking,ModestyLevel,NumberOfBelongings")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(form);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreditHourId"] = new SelectList(_context.CreditHours, "CreditHourId", "CreditHourName", form.CreditHourId);
+            ViewData["DormId"] = new SelectList(_context.Dorms, "DormId", "DormName", form.DormId);
+            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorName", form.MajorId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", form.UserId);
+            return View(form);
+        }
+
+        // GET: Forms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Forms == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _context.Forms.FindAsync(id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreditHourId"] = new SelectList(_context.CreditHours, "CreditHourId", "CreditHourName", form.CreditHourId);
+            ViewData["DormId"] = new SelectList(_context.Dorms, "DormId", "DormName", form.DormId);
+            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorName", form.MajorId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", form.UserId);
+            return View(form);
+        }
+
+        // POST: Forms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FormId,DormId,UserId,MajorId,CreditHourId,GenderId,IsCandiateStudent,IsStudentAthlete,NeatnessLevel,VisitorLevel,FitnessLevel,AcademicLevel,SocialLevel,SharingLevel,BackgroundNoiseLevel,BedTimeRanking,ModestyLevel,NumberOfBelongings")] Form form)
+        {
+            if (id != form.FormId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(form);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FormExists(form.FormId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreditHourId"] = new SelectList(_context.CreditHours, "CreditHourId", "CreditHourName", form.CreditHourId);
+            ViewData["DormId"] = new SelectList(_context.Dorms, "DormId", "DormName", form.DormId);
+            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorName", form.MajorId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", form.UserId);
+            return View(form);
+        }
+
+        // GET: Forms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Forms == null)
+            {
+                return NotFound();
+            }
+
+            var form = await _context.Forms
+                .Include(f => f.CreditHour)
+                .Include(f => f.Dorm)
+                .Include(f => f.Major)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.FormId == id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+
+            return View(form);
+        }
+
+        // POST: Forms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Forms == null)
+            {
+                return Problem("Entity set 'HallHavenContext.Forms'  is null.");
+            }
+            var form = await _context.Forms.FindAsync(id);
+            if (form != null)
+            {
+                _context.Forms.Remove(form);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FormExists(int id)
+        {
+          return (_context.Forms?.Any(e => e.FormId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HallHaven.Data;
+using HallHaven.Models;
+using Microsoft.AspNetCore.Identity;
+using HallHaven.Areas.Identity.Data;
+
+namespace HallHaven.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly HallHavenContext _context;
+        UserManager<HallHavenUser> _userManager;
+
+        public UserController(HallHavenContext context, UserManager<HallHavenUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            // get current user id
+            //var userId = User.GetLoggedInUserId<string>();
+            // add current user id to user
+
+            var user = await _userManager.GetUserAsync(User);
+            // get logged in user's gender
+            var gender = user.Gender;
+
+            var hallHavenContext = _context.Users.Include(u => u.Gender);
+            return View(await hallHavenContext.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Gender)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            var userId = User.GetLoggedInUserId<string>();
+
+
+            ViewData["GenderId"] = new SelectList(_context.Genders, "GenderId", "GenderName");
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,FirstName,LastName,GenderId,ProfilePicture,ProfileBio,DisplayName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenderId"] = new SelectList(_context.Genders, "GenderId", "GenderName", user.GenderId);
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenderId"] = new SelectList(_context.Genders, "GenderId", "GenderName", user.GenderId);
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("UserId,FirstName,LastName,GenderId,ProfilePicture,ProfileBio,DisplayName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenderId"] = new SelectList(_context.Genders, "GenderId", "GenderName", user.GenderId);
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Gender)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'HallHavenContext.Users'  is null.");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(string id)
+        {
+          return (_context.Users?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+    }
+}
