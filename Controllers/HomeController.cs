493545238cdd258410ba638d3892211c98bafea8
Kiah Daulton <59930638+kiahdaulton@users.noreply.github.com>
2023-03-31 12:58:41 -0500

    Query Registered Users by Gender
    Only display users by the gender of the logged in user

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using HallHaven.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Security.Claims;
 using static HallHaven.Models.ClaimsPrincipalExtensions;
 
@@ -22,23 +23,31 @@ namespace HallHaven.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-
-           // var userId = _userManager.GetUserId(HttpContext.User);
-
+            // current user Guid
             var userId = User.GetLoggedInUserId<string>();
-            //var userGender = _userManager.GetLoggedInUserGender(User);
-            //var users = GetUsers();
-            // get all users
-            //var users = _context.Users.ToList();
-            //var user = await _userManager.GetUserAsync(User);
-            // get gender of logged in user
-            // display roommates if logged in user gender equals gender of each user in the list
-            //return View();
 
-            var users = _userManager.Users.ToList();
-            return View(users);
+            // if user is logged in
+            if (userId != null)
+            {
+                // get logged in user
+                var user = await _userManager.GetUserAsync(User);
+                // get logged in user's gender
+                var gender = user.Gender;
+                // only show users that match the logged in user's gender in the view
+                var users = _userManager.Users.Where(g => g.Gender == gender).ToList();
+
+                // configure not displaying logged in user or add a specific button to current user to hide profile
+
+
+                return View(users);
+            }
+            else
+            {
+                // display default view if user is not currently logged in
+                return View();
+            }     
 
             // this gets and displays username/email
             //return Content(this.User.GetLoggedInUserName());
@@ -50,19 +59,19 @@ namespace HallHaven.Controllers
             return View(users);
         }
 
-        public IActionResult GetUsers()
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
-            var userName = User.FindFirstValue(ClaimTypes.Name); // will give the user's userName
-            var userEmail = User.FindFirstValue(ClaimTypes.Email); // will give the user's Email
-            var userGender = User.FindFirstValue(ClaimTypes.Gender); // get user gender
+        //public IActionResult GetUsers()
+        //{
+        //    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
+        //    var userName = User.FindFirstValue(ClaimTypes.Name); // will give the user's userName
+        //    var userEmail = User.FindFirstValue(ClaimTypes.Email); // will give the user's Email
+        //    var userGender = User.FindFirstValue(ClaimTypes.Gender); // get user gender
 
-            // use extension class 
-            var userId2 = User.GetLoggedInUserId<string>(); // Specify the type of your UserId;
-            var userName2 = User.GetLoggedInUserName();
-            var userEmail2 = User.GetLoggedInUserEmail();
-            return View(userId);
-        }
+        //    // use extension class 
+        //    var userId2 = User.GetLoggedInUserId<string>(); // Specify the type of your UserId;
+        //    var userName2 = User.GetLoggedInUserName();
+        //    var userEmail2 = User.GetLoggedInUserEmail();
+        //    return View(userId);
+        //}
 
         public IActionResult Privacy()
         {
@@ -76,18 +85,18 @@ namespace HallHaven.Controllers
         }
     }
 
-    public class OtherClass
-    {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        public OtherClass(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
+    //public class OtherClass
+    //{
+    //    private readonly IHttpContextAccessor _httpContextAccessor;
+    //    public OtherClass(IHttpContextAccessor httpContextAccessor)
+    //    {
+    //        _httpContextAccessor = httpContextAccessor;
+    //    }
 
-        public void GetUsers()
-        {
-            //var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var userId = _httpContextAccessor.HttpContext.User.GetLoggedInUserId<string>(); // Specify the type of your UserId;
-        }
-    }
+    //    public void GetUsers()
+    //    {
+    //        //var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+    //        var userId = _httpContextAccessor.HttpContext.User.GetLoggedInUserId<string>(); // Specify the type of your UserId;
+    //    }
+    //}
 }
