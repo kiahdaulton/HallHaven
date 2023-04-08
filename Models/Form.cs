502bc665f3622116e385a4137c587f55165b40c9
Kiah Daulton <59930638+kiahdaulton@users.noreply.github.com>
2023-04-08 12:07:02 -0500

    Add Empty Form on Registration
    Create form with same userId in User table upon registration

--- Models/Form.cs
+++ Models/Form.cs
@@ -11,18 +11,18 @@ namespace HallHaven.Models
         public int? MajorId { get; set; }
         public int? CreditHourId { get; set; }
         public int? GenderId { get; set; }
-        public bool IsCandiateStudent { get; set; }
-        public bool IsStudentAthlete { get; set; }
-        public int NeatnessLevel { get; set; }
-        public int VisitorLevel { get; set; }
-        public int FitnessLevel { get; set; }
-        public int AcademicLevel { get; set; }
-        public int SocialLevel { get; set; }
-        public int SharingLevel { get; set; }
-        public int BackgroundNoiseLevel { get; set; }
-        public int BedTimeRanking { get; set; }
-        public int ModestyLevel { get; set; }
-        public int NumberOfBelongings { get; set; }
+        public bool? IsCandiateStudent { get; set; }
+        public bool? IsStudentAthlete { get; set; }
+        public int? NeatnessLevel { get; set; }
+        public int? VisitorLevel { get; set; }
+        public int? FitnessLevel { get; set; }
+        public int? AcademicLevel { get; set; }
+        public int? SocialLevel { get; set; }
+        public int? SharingLevel { get; set; }
+        public int? BackgroundNoiseLevel { get; set; }
+        public int? BedTimeRanking { get; set; }
+        public int? ModestyLevel { get; set; }
+        public int? NumberOfBelongings { get; set; }
 
         public virtual CreditHour CreditHour { get; set; } = null!;
         public virtual Dorm Dorm { get; set; } = null!;
